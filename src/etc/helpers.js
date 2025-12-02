c79f8d1fa61430a73444754f2cc89f93586ee4fa
dohyeon5626 <kodohyeon71@gmail.com>
2025-12-02 12:47:42 +0900

    Update JPEG quality in reSizeImageUrl function
    Increase JPEG quality for resized images from 0.8 to 0.95.

--- src/etc/helpers.js
+++ src/etc/helpers.js
@@ -71,7 +71,7 @@ export const copyToClipboard = async (text) => {
 
 export const reSizeImageUrl = (file) => {
     const MAX_DIMENSION = 400;
-    const JPEG_QUALITY = 0.8;
+    const JPEG_QUALITY = 0.95;
 
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
